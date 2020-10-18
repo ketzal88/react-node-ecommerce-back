f10b916f1630ab4fecf6469b7b618d0e37c8ea3e
Gabriel Uccello <gabrielucc@gmail.com>
2020-10-18 01:17:10 -0300

    endpoint by search by name and by category

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+function findProducts(search, products) {
+  return products.filter(function (el, i) {
+    return el.name.toLowerCase().indexOf(search.toLowerCase()) > -1;
+  });
+}
+
+function findProductsByCategory(category, products) {
+  return products.filter(function (el, i) {
+    return el.categoria.toLowerCase().indexOf(category.toLowerCase()) > -1;
+  });
+}
+
 ///// Endpoints ////
 
 ///// Endpoint All Products ////
@@ -19,67 +31,18 @@ app.get("/products", function (req, res) {
   res.status(200).send({ products });
 });
 
-///// Endpoint Single Product by ID ////
-app.get("/products/:id", function (req, res) {
-  axios
-    .get("https://rapidapi.p.rapidapi.com/catalog/product/" + req.params.id, {
-      headers,
-    })
-    .then((response) => {
-      res.send({ product: response.data });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+///// Endpoint Product by search param ////
+app.get("/products/:search", function (req, res) {
+  const search = req.params.search;
+  const product = findProducts(search, products.products);
+  res.status(200).send({ product });
 });
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+///// Endpoint Products by category ////
+app.get("/products/category/:category", function (req, res) {
+    const category = req.params.category;
+    const product = findProductsByCategory(category, products.products);
+    res.status(200).send({ product });
+  });
 
-// function createImgLink(allPokemons) {
-//   allPokemons.forEach(function (el, i) {
-//     let index = i + 1;
-//     el.img =
-//       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-//       index +
-//       ".png";
-//   });
-//   return allPokemons;
-// }
-
-// function findPokemons(inputValue, allPokemons) {
-//   return allPokemons.filter(function (el, i) {
-//     return el.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1;
-//   });
-// }
-
-// app.get("/search/:search", function (req, res) {
-//   axios
-//     .get("https://pokeapi.co/api/v2/pokemon?limit=500")
-//     .then((response) => {
-//       let allPokemons = response.data.results;
-//       let inputValue = req.params.search;
-
-//       let searchResultWithImg = createImgLink(allPokemons);
-//       let searchResult = findPokemons(inputValue, searchResultWithImg);
-//       res.send({ results: searchResult });
-//     })
-//     .catch((error) => {
-//       return res.status(400).send({
-//         error: error.response.data.error,
-//       });
-//     });
-// });
-
-// app.get("/", function (req, res) {
-//   axios
-//     .then((res) => console.log(productos))
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
-
-module.exports = app; // for testing
+module.exports = app;
