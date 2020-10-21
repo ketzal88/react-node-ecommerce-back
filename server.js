9c3c887747e0d6fbca120fd72e03e28a8ff1b69c
Gabriel Uccello <gabrielucc@gmail.com>
2020-10-20 21:20:10 -0300

    push final

--- server.js
+++ server.js
@@ -14,52 +14,79 @@ app.use(bodyParser.json());
 ///// Endpoint All Products ////
 
 app.get("/products", function (req, res) {
-  res.status(200).send({ products });
-});
+  let productosFinal = products.products;
 
-///// Endpoint Product by search param ////
-app.get("/products/:search", function (req, res) {
-  const search = req.params.search;
-  function findProducts(search, products) {
-    return products.filter(function (el, i) {
-      return el.name.toLowerCase().indexOf(search.toLowerCase()) > -1;
+  const currentPage = req.query.page || 0;
+  const search = req.query.search || "";
+  const categories = req.query.category.split(",") || [];
+  const min = req.query.priceMin || 0;
+  const max = req.query.priceMax || 1000000;
+  const limit = 10;
+
+  /// Search
+  if (search) {
+    productosFinal = productosFinal.filter(function (el, i) {
+      return el.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
     });
   }
-  const product = findProducts(search, products.products);
-  res.status(200).send({ product });
+
+  // Category
+  if (categories.length > 0) {
+    let allCategories = [];
+    categories.forEach(function (el) {
+      console.log(el);
+      allCategories = [
+        ...allCategories,
+        ...findProductsByCategory(el, productosFinal),
+      ];
+    });
+    productosFinal = [...new Set(allCategories)];
+  }
+
+  // Price
+  if (min || max) {
+    productosFinal = productosFinal.filter(
+      (item) => item.precio >= min && item.precio <= max
+    );
+  }
+
+  // Paginacion
+
+  const productosFinal2 = productosFinal.slice(
+    currentPage * limit,
+    currentPage * limit + limit
+  );
+
+  // Total en base a los productos luego del filtro
+
+  const total = productosFinal.length;
+
+  // Calculo de las paginas luego del filtro
+  const pages = Math.round(total / limit) + 1;
+
+  // Respuesta luego de los filtros
+
+  productos = productosFinal2;
+
+  res.status(200).send({ productos, pages });
 });
 
 ///// Endpoint Product by ID ////
 app.get("/product/:id", function (req, res) {
   const id = req.params.id;
   function findProduct(id, products) {
-    return products.filter(produ => id.includes(produ.id)).map(produ => produ)
+    return products
+    .filter((produ) => id.includes(produ.id))
+    .map((produ) => produ);
   }
   const product = findProduct(id, products.products);
   res.status(200).send({ product });
 });
 
-///// Endpoint Products by category ////
-app.get("/products/category/:category", function (req, res) {
-  const category = req.params.category;
-  function findProductsByCategory(category, products) {
-    return products.filter(function (el, i) {
-      return el.categoria.toLowerCase().indexOf(category.toLowerCase()) > -1;
-    });
-  }
-  const product = findProductsByCategory(category, products.products);
-  res.status(200).send({ product });
-});
-
-///// Endpoint Products by price min and max ////
-app.get("/products/price/:min/:max", function (req, res) {
-  const min = req.params.min;
-  const max = req.params.max;
-  function findMinMax(products, min, max) {
-    return products.filter((item) => item.precio >= min && item.precio <= max);
-  }
-  const product = findMinMax(products.products, min, max);
-  res.status(200).send({ product });
-});
+function findProductsByCategory(category, products) {
+  return products.filter(function (el, i) {
+    return el.categoria.toLowerCase().indexOf(category.toLowerCase()) > -1;
+  });
+}
 
 module.exports = app;
